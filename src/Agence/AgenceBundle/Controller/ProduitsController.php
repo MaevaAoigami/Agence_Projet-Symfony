144b9758b807ea244cc661afdeb5b81ee77acd72
MaevaAoigami <ridardmaeva@gmail.com>
2015-03-06 16:45:45 +0100

    Add Pagination

--- src/Agence/AgenceBundle/Controller/ProduitsController.php
+++ src/Agence/AgenceBundle/Controller/ProduitsController.php
@@ -11,12 +11,18 @@ class ProduitsController extends Controller
         $session = $this->getRequest()->getSession();
         $em = $this->getDoctrine()->getManager();
         
-        $danseuses = $em->getRepository('AgenceBundle:Danseuses')->findBy(array('disponible' => 1));
+        $findDanseuses = $em->getRepository('AgenceBundle:Danseuses')->findBy(array('disponible' => 1));
         if ($session->has('panier'))
         	$panier = $session->get('panier');
         else 
         	$panier = false;
 
+        $danseuses  = $this->get('knp_paginator');
+        $danseuses = $danseuses->paginate(
+            $findDanseuses,
+            $this->get('request')->query->get('page', 1)/*page number*/,
+            6/*limit per page*/
+        );
         
         return $this->render('AgenceBundle:Default:produits/layout/produits.html.twig', array('danseuses' => $danseuses,
                                                                                               'panier' => $panier));
