6f61ce597b329d1ee132a9e1daf28e00b1b0dbbd
MaevaAoigami <ridardmaeva@gmail.com>
2015-03-12 14:22:17 +0100

    Bug fix Media

--- src/Agence/AgenceBundle/Entity/Media.php
+++ src/Agence/AgenceBundle/Entity/Media.php
@@ -3,6 +3,7 @@
 namespace Agence\AgenceBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\validator\Constraints as Assert;
 
 /**
  * Media
@@ -12,7 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Media
 {
-    /**
+     /**
      * @var integer
      *
      * @ORM\Column(name="id", type="integer")
@@ -20,35 +21,92 @@ class Media
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     private $id;
+
+    /**
+     * @var \DateTime
+     * 
+     * @ORM\COlumn(name="updated_at",type="datetime", nullable=true) 
+     */
+    private $updateAt;
     
     /**
-     * @ORM\ManyToOne(targetEntity="Agence\AgenceBundle\Entity\Danseuses", inversedBy="media")
-     * @ORM\JoinColumn(nullable=true)
+     * @ORM\PostLoad()
      */
-    private $danseuse;
-
+    public function postLoad()
+    {
+        $this->updateAt = new \DateTime();
+    }
+    
     /**
-     * @var string
-     *
-     * @ORM\Column(name="path", type="string", length=255)
+     * @ORM\Column(type="string",length=255) 
+     * @Assert\NotBlank
+     */
+    private $name;
+    
+    /**
+     * @ORM\Column(type="string",length=255, nullable=true) 
      */
     private $path;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="alt", type="string", length=255)
-     */
-    private $alt;
-
-    /**
-     * Transform to string
-     * 
-     * @return string
-     */
-    public function __toString()
+    
+    public $file;
+    
+    public function getUploadRootDir()
     {
-        return (string) $this->getId();
+        return __dir__.'/../../../../web/uploads';
+    }
+    
+    public function getAbsolutePath()
+    {
+        return null === $this->path ? null : $this->getUploadRootDir().'/'.$this->path;
+    }
+    
+    public function getAssetPath()
+    {
+        return 'uploads/'.$this->path;
+    }
+    
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate() 
+     */
+    public function preUpload()
+    {
+        $this->tempFile = $this->getAbsolutePath();
+        $this->oldFile = $this->getPath();
+        $this->updateAt = new \DateTime();
+        
+        if (null !== $this->file) 
+            $this->path = sha1(uniqid(mt_rand(),true)).'.'.$this->file->guessExtension();
+    }
+    
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate() 
+     */
+    public function upload()
+    {
+        if (null !== $this->file) {
+            $this->file->move($this->getUploadRootDir(),$this->path);
+            unset($this->file);
+            
+            if ($this->oldFile != null) unlink($this->tempFile);
+        }
+    }
+    
+    /**
+     * @ORM\PreRemove() 
+     */
+    public function preRemoveUpload()
+    {
+        $this->tempFile = $this->getAbsolutePath();
+    }
+    
+    /**
+     * @ORM\PostRemove() 
+     */
+    public function removeUpload()
+    {
+        if (file_exists($this->tempFile)) unlink($this->tempFile);
     }
 
     /**
@@ -60,73 +118,63 @@ class Media
     {
         return $this->id;
     }
+    
+    public function getPath()
+    {
+        return $this->path;
+    }
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set updateAt
+     *
+     * @param \DateTime $updateAt
+     * @return Media
+     */
+    public function setUpdateAt($updateAt)
+    {
+        $this->updateAt = $updateAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updateAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdateAt()
+    {
+        return $this->updateAt;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Media
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 
     /**
      * Set path
      *
      * @param string $path
-     * @return media
+     * @return Media
      */
     public function setPath($path)
     {
         $this->path = $path;
 
         return $this;
-    }
-
-    /**
-     * Get path
-     *
-     * @return string 
-     */
-    public function getPath()
-    {
-        return $this->path;
-    }
-
-    /**
-     * Set alt
-     *
-     * @param string $alt
-     * @return media
-     */
-    public function setAlt($alt)
-    {
-        $this->alt = $alt;
-
-        return $this;
-    }
-
-    /**
-     * Get alt
-     *
-     * @return string 
-     */
-    public function getAlt()
-    {
-        return $this->alt;
-    }
-
-    /**
-     * Set danseuse
-     *
-     * @param \Agence\AgenceBundle\Entity\Danseuses $danseuse
-     * @return Media
-     */
-    public function setDanseuse(\Agence\AgenceBundle\Entity\Danseuses $danseuse = null)
-    {
-        $this->danseuse = $danseuse;
-
-        return $this;
-    }
-
-    /**
-     * Get danseuse
-     *
-     * @return \Agence\AgenceBundle\Entity\Danseuses 
-     */
-    public function getDanseuse()
-    {
-        return $this->danseuse;
     }
 }
